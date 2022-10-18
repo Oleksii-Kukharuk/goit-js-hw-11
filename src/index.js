1f5afc79895b184f729df8f4566a83efa7ecde6d
Oleksii-Kukharuk <104523043+Oleksii-Kukharuk@users.noreply.github.com>
2022-10-18 21:24:07 +0300

    button and markup - fixed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// import { createMarkup } from './js/markup';
+import { createMarkup } from './js/markup';
 import ImageApiServer from './js/axios';
-// const axios = require('axios').default;
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { createMarkup } from './js/markup';
 
 const imageApiServer = new ImageApiServer();
 
@@ -15,47 +13,36 @@ const loadBtnRef = document.querySelector('.load-more');
 inputFormRef.addEventListener('submit', submitHandler);
 loadBtnRef.addEventListener('click', loadMoreHandler);
 
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionType: 'alt',
+  captionsData: 'alt',
+  captionDelay: 250,
+  showCounter: false,
+  maxZoom: 2,
+  scrollZoomFactor: 0.1,
+});
+
 function submitHandler(e) {
-  let lightbox = new SimpleLightbox('.gallery a', {
-    captionType: 'alt',
-    captionsData: 'alt',
-    captionDelay: 250,
-    showCounter: false,
-    maxZoom: 2,
-    scrollZoomFactor: 0.1,
-  });
   e.preventDefault();
   imageApiServer.query = e.currentTarget.elements.searchQuery.value;
-
   galleryRef.innerHTML = '';
   imageApiServer.resetPage();
   imageApiServer.fetchPhoto().then(data => {
-    data.map(
-      ({
-        webformatURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-        largeImageURL,
-      }) => {
-        const markup = createMarkup({
-          webformatURL,
-          tags,
-          likes,
-          views,
-          comments,
-          downloads,
-          largeImageURL,
-        });
-        galleryRef.insertAdjacentHTML('beforeend', markup);
-        lightbox.refresh();
-      }
-    );
+    data.map(data => {
+      galleryRef.insertAdjacentHTML('beforeend', createMarkup(data));
+      lightbox.refresh();
+    });
+    Notify.success('look what I found for you');
+    loadBtnRef.classList.remove('visually-hidden');
   });
 }
 
 function loadMoreHandler() {
-  imageApiServer.fetchPhoto();
+  imageApiServer.fetchPhoto().then(data => {
+    data.map(data => {
+      galleryRef.insertAdjacentHTML('beforeend', createMarkup(data));
+      lightbox.refresh();
+    });
+    Notify.success('here you go');
+  });
 }
